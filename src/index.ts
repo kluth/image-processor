b69df50b5361beb5250f9aba870621923ccb315b
Matthias Kluth <km@paibosoft.de>
2022-06-07 22:41:43 +0200

    feat(jasmine): configured Jasmine is configured to test the app properly

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+export const add = (a: number, b: number): number => {
+    return a + b
+}
