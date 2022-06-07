0423e819ed9a5b6fefd8b243cb909215727d3843
Matthias Kluth <km@paibosoft.de>
2022-06-07 21:34:52 +0200

    initial setup continued

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-    })
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
 
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000')
-    })
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
